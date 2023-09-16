abea2af5d7a9ecea62273f495252860f2f0a015b
benjaAguilar <benjaaguilar02@gmail.com>
2023-09-16 15:15:50 -0300

    Added new functions to the new note button
    now you can hide and unhide the new note section
    when you click the button a note is created on the note section

--- js/toDoScript.js
+++ js/toDoScript.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    console.log("jquery charged");
+    let newNoteSection = $(".new-note");
+    let newNoteClick = false;
+
+    newNoteSection.css("display", "none");
+
+    $("#create-note").click(function(){
+        if(newNoteClick === false){
+            newNoteSection.css("display", "flex");
+            newNoteClick = true;
+
+        }else{
+            newNoteSection.css("display", "none");
+            newNoteClick = false;
+
+        }
+        let note = $("<div>").addClass("note");
+        let noteTitle = $("<div>").append("New note").addClass("note-title");
+        let noteData = $("<ul>").addClass("note-info");
+
+        note.append(noteTitle);
+        note.append(noteData);
+
+        $(".notes-section").append(note);
+    });
 });
